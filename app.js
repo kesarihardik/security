9dca39117211f71531f317ba4e2874c41a2fc974
astronaut <you@example.com>
2021-05-28 16:07:24 +0530

    First level authentication using mongoose

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.urlencoded({extended:true}));  //since bodyparser deprecated. no need to install bodyparser now
+app.use(express.json());
+
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+mongoose.connect('mongodb://localhost/userDB',{useNewUrlParser:true,useUnifiedTopology:true});
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model('User',userSchema);
+
+app.get('/',(req,res)=>{
+    res.render('home');
+});
+
+app.get('/login',(req,res)=>{
+    res.render('login');
+});
+
+app.get('/register',(req,res)=>{
+    res.render('register');
+});
+
+
+app.post('/register',(req,res)=>{ 
+    
+    const newUser = new User({
+     email: req.body.username,
+     password: req.body.password
+    });
+
+    newUser.save((err)=>{
+        if(err)
+        console.log(err);
+        else
+         res.render('secrets');
+    });
+});
+
+app.post('/login',(req,res)=>{
+
+    User.findOne({email:req.body.username},(err,founduser)=>{
+     if(!err){
+       if(founduser.password === req.body.password){
+           res.render("secrets");
+       }
+        else
+         res.send('Oops! You have entered an invalid username or password');
+       }
+     else{
+        console.log(err);
+     }
+      
+    });
+});
+
+app.listen('3000',()=>{
+    console.log('Server started on port 3000');
+})
